643e95fdd747a79429482e0820a124acc5581c79
Yantivenom <91446333+Yantivenom@users.noreply.github.com>
2022-11-26 23:24:01 -0800

    UPDATE 1.4
    Changes made in the More Platforms 1.4 Update.

---
+++ Content/Items/InfusedHoneyPlatform.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MorePlatforms.Content.Items
+{
+	public class InfusedHoneyPlatform : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Infused Honey Platform");
+			Tooltip.SetDefault("Just like swimming in honey!");
+		}
+		public override void SetDefaults()
+		{
+			Item.width = 12;
+			Item.height = 12;
+			Item.maxStack = 999;
+			Item.useTurn = true;
+			Item.useTime = 1;
+			Item.useAnimation = 15;
+			Item.autoReuse = true;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.createTile = ModContent.TileType<Tiles.InfusedHoneyPlatform>();
+
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe(1)
+			.AddIngredient(ItemID.HoneyPlatform)
+			.AddTile(TileID.HoneyDispenser)
+			.Register();
+
+			Recipe decraftRecipe = Recipe.Create(ItemID.HoneyPlatform);
+			decraftRecipe.AddIngredient<InfusedHoneyPlatform>(1);
+			decraftRecipe.AddTile(TileID.HoneyDispenser);
+			decraftRecipe.Register();
+
+		}
+	}
+}
---
+++ Content/Tiles/InfusedHoneyPlatform.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MorePlatforms.Content.Tiles
+{
+	public class InfusedHoneyPlatform : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileLighted[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileSolidTop[Type] = true;
+			Main.tileSolid[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileTable[Type] = true;
+			Main.tileLavaDeath[Type] = false;
+			Main.tileWaterDeath[Type] = false;
+			TileID.Sets.Platforms[Type] = true;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16 };
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleMultiplier = 27;
+			TileObjectData.newTile.StyleWrapLimit = 27;
+			TileObjectData.newTile.UsesCustomCanPlace = false;
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.addTile(Type);
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsDoor);
+			AddMapEntry(new Color(47, 51, 58));
+			DustType = 54;
+			ItemDrop = ModContent.ItemType<Items.AsphaltPlatformItem>();
+			AdjTiles = new int[] { TileID.Platforms };
+		}
+		public override void PostSetDefaults()
+		{
+			Main.tileNoSunLight[Type] = false;
+		}
+		public override void NumDust(int i, int j, bool fail, ref int num)
+		{
+			num = 10;
+		}
+		public override void FloorVisuals(Player player)
+		{
+			player.AddBuff(BuffID.Honey, 60);
+		}
+	}
+}
