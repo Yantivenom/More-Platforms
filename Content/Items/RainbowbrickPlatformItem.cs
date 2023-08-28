3c53c508200109ef910cb5e31be219525881338e
Yantivenom <91446333+Yantivenom@users.noreply.github.com>
2023-08-27 21:38:38 -0400

    Update 1.6
    Added Rainbowbrick Platforms

---
+++ Content/Items/RainbowbrickPlatformItem.cs
@@ -0,0 +1,59 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace MorePlatforms.Content.Items
+{
+    public class RainbowbrickPlatformItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 200;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 8;
+            Item.height = 10;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 1;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<Tiles.RainbowbrickPlatform>();
+        }
+
+        public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            base.PostDrawInInventory(spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            Item.color = Main.DiscoColor;
+        }
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+        {
+            base.PostDrawInWorld(spriteBatch, lightColor, alphaColor, rotation, scale, whoAmI);
+            Item.color = Main.DiscoColor;
+        }
+
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(2)
+                .AddIngredient(ItemID.RainbowBrick)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+
+            Recipe decraftRecipe = Recipe.Create(ItemID.RainbowBrick);
+            decraftRecipe.AddIngredient<RainbowbrickPlatformItem>(2);
+            decraftRecipe.Register();
+        }
+    }
+}
---
+++ Content/Tiles/RainbowbrickPlatform.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace MorePlatforms.Content.Tiles
+{
+    public class RainbowbrickPlatform : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			//Properties
+			Main.tileLighted[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileSolidTop[Type] = true;
+			Main.tileSolid[Type] = true;
+			Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+            TileID.Sets.Platforms[Type] = true;
+			TileID.Sets.DisableSmartCursor[Type] = true;
+
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsDoor);
+			AddMapEntry(new Color(58, 0, 29));
+
+			AdjTiles = new int[] { TileID.Platforms };
+
+			//Placeable
+			TileObjectData.newTile.CoordinateHeights = new[] { 16 };
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.StyleMultiplier = 27;
+			TileObjectData.newTile.StyleWrapLimit = 27;
+			TileObjectData.newTile.UsesCustomCanPlace = false;
+			TileObjectData.newTile.LavaDeath = true;
+			TileObjectData.addTile(Type);
+		}
+
+        public override void PostSetDefaults() => Main.tileNoSunLight[Type] = false;
+
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+        {
+			drawData.colorTint = Color.White;
+			drawData.tileLight = Main.DiscoColor;
+        }
+    }
+}
