ecdc75a08d5a530295a67c95de1447b864bda324
Yantivenom <91446333+Yantivenom@users.noreply.github.com>
2023-12-05 21:30:23 -0500

    UPDATE 1.7
    CHRISTMAS UPDATE
    CandyCane Platform
    Green CandyCane Platform
    Stone Platforms now called CobbleStone Platforms since Stone Platform is a vanilla item
    
    HAPPY HOLIDAYS

---
+++ Content/Items/CandyCanePlatformItem.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace MorePlatforms.Content.Items
+{
+    public class CandyCanePlatformItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Tooltip.SetDefault("Red jolly platforms.");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 200;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 8;
+            Item.height = 10;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 1;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<Tiles.CandyCanePlatform>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(2)
+                .AddIngredient(ItemID.CandyCaneBlock)
+                .Register();
+
+            Recipe decraftRecipe = Recipe.Create(ItemID.CandyCaneBlock);
+            decraftRecipe.AddIngredient<CandyCanePlatformItem>(2);
+            decraftRecipe.Register();
+        }
+    }
+}
